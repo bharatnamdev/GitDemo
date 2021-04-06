01a7dc2e0e1baf88b6710f4519ab037b85b449c1
bharat <bharat.namdev@gmail.com>
2021-04-06 16:21:02 -0400

    Initial commit.

---
+++ target/cucumber/report.js
@@ -0,0 +1,495 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Account.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Portal Login",
+  "description": "",
+  "id": "portal-login",
+  "keyword": "Feature"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "validate the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Browser is triggered",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "check if browser is started",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefination.validate_the_browser()"
+});
+formatter.result({
+  "duration": 3629498309,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.browser_is_triggered()"
+});
+formatter.result({
+  "duration": 136096,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.check_if_browser_is_started()"
+});
+formatter.result({
+  "duration": 133865,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 9,
+  "name": "Home page default login",
+  "description": "",
+  "id": "portal-login;home-page-default-login",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 8,
+      "name": "@PortalTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 10,
+  "name": "User is on NetBanking landing page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "User login into application with \"jin\" and password \"1234\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "Home page is populated",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "Cards displayed are \"true\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefination.user_is_on_netbanking_landing_page()"
+});
+formatter.result({
+  "duration": 141005,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "jin",
+      "offset": 34
+    },
+    {
+      "val": "1234",
+      "offset": 53
+    }
+  ],
+  "location": "stepDefination.user_login_into_application_with_something_and_password_something(String,String)"
+});
+formatter.result({
+  "duration": 5765119,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.home_page_is_populated()"
+});
+formatter.result({
+  "duration": 153498,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "true",
+      "offset": 21
+    }
+  ],
+  "location": "stepDefination.cards_displayed_are(String)"
+});
+formatter.result({
+  "duration": 217308,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "validate the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Browser is triggered",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "check if browser is started",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefination.validate_the_browser()"
+});
+formatter.result({
+  "duration": 166439,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.browser_is_triggered()"
+});
+formatter.result({
+  "duration": 169116,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.check_if_browser_is_started()"
+});
+formatter.result({
+  "duration": 111108,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "Home page default login",
+  "description": "",
+  "id": "portal-login;home-page-default-login",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 15,
+      "name": "@PortalTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 17,
+  "name": "User is on NetBanking landing page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "User login into application with \"john\" and password \"4321\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 19,
+  "name": "Home page is populated",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "Cards displayed are \"false\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefination.user_is_on_netbanking_landing_page()"
+});
+formatter.result({
+  "duration": 215522,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "john",
+      "offset": 34
+    },
+    {
+      "val": "4321",
+      "offset": 54
+    }
+  ],
+  "location": "stepDefination.user_login_into_application_with_something_and_password_something(String,String)"
+});
+formatter.result({
+  "duration": 285579,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.home_page_is_populated()"
+});
+formatter.result({
+  "duration": 94598,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "false",
+      "offset": 21
+    }
+  ],
+  "location": "stepDefination.cards_displayed_are(String)"
+});
+formatter.result({
+  "duration": 291825,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "validate the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Browser is triggered",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "check if browser is started",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefination.validate_the_browser()"
+});
+formatter.result({
+  "duration": 161530,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.browser_is_triggered()"
+});
+formatter.result({
+  "duration": 107984,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.check_if_browser_is_started()"
+});
+formatter.result({
+  "duration": 102183,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 23,
+  "name": "Home page default login",
+  "description": "",
+  "id": "portal-login;home-page-default-login",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 22,
+      "name": "@PortalTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 24,
+  "name": "User is on NetBanking landing page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 25,
+  "name": "User login into application with \"john\" and password \"4321\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 26,
+  "name": "Home page is populated",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 27,
+  "name": "Cards displayed are \"false\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefination.user_is_on_netbanking_landing_page()"
+});
+formatter.result({
+  "duration": 144574,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "john",
+      "offset": 34
+    },
+    {
+      "val": "4321",
+      "offset": 54
+    }
+  ],
+  "location": "stepDefination.user_login_into_application_with_something_and_password_something(String,String)"
+});
+formatter.result({
+  "duration": 303874,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.home_page_is_populated()"
+});
+formatter.result({
+  "duration": 112893,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "false",
+      "offset": 21
+    }
+  ],
+  "location": "stepDefination.cards_displayed_are(String)"
+});
+formatter.result({
+  "duration": 182057,
+  "status": "passed"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "validate the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Browser is triggered",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "check if browser is started",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepDefination.validate_the_browser()"
+});
+formatter.result({
+  "duration": 159299,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.browser_is_triggered()"
+});
+formatter.result({
+  "duration": 107538,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.check_if_browser_is_started()"
+});
+formatter.result({
+  "duration": 128957,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 29,
+  "name": "Home page default login",
+  "description": "",
+  "id": "portal-login;home-page-default-login",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 28,
+      "name": "@PortalTest"
+    }
+  ]
+});
+formatter.step({
+  "line": 30,
+  "name": "User is on NetBanking landing page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 31,
+  "name": "User sign up with following details",
+  "rows": [
+    {
+      "cells": [
+        "jenny",
+        "abcd",
+        "john@abcd.com",
+        "Australia",
+        "3242353"
+      ],
+      "line": 32
+    }
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 33,
+  "name": "Home page is populated",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 34,
+  "name": "Cards displayed are \"false\"",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepDefination.user_is_on_netbanking_landing_page()"
+});
+formatter.result({
+  "duration": 105753,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.user_sign_up_with_following_details(DataTable)"
+});
+formatter.result({
+  "duration": 3004377,
+  "status": "passed"
+});
+formatter.match({
+  "location": "stepDefination.home_page_is_populated()"
+});
+formatter.result({
+  "duration": 223109,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "false",
+      "offset": 21
+    }
+  ],
+  "location": "stepDefination.cards_displayed_are(String)"
+});
+formatter.result({
+  "duration": 194550,
+  "status": "passed"
+});
+});
